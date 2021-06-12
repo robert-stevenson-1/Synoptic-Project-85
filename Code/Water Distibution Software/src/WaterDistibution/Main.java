aee22345b60ef3b7dd7bf484433cb144a817caf6
IPEKMERAL1 <80692388+IPEKMERAL1@users.noreply.github.com>
2021-06-12 22:09:54 +0300

    test change

--- Code/Water Distibution Software/src/WaterDistibution/Main.java
+++ Code/Water Distibution Software/src/WaterDistibution/Main.java
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class Main extends Application {
-
+//test
     @Override
     public void start(Stage stage) throws Exception {
 
