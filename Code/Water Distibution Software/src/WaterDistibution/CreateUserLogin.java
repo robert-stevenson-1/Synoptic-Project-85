372fc86d23cd5540f6b0efe51fe66e25fd29e468
Sweena <brb18jsu@uea.ac.uk>
2021-06-13 14:53:43 +0100

    tetsin

--- Code/Water Distibution Software/src/WaterDistibution/CreateUserLogin.java
+++ Code/Water Distibution Software/src/WaterDistibution/CreateUserLogin.java
@@ -1,36 +1,62 @@
 package WaterDistibution;
 
+/**
+ * Class: User Login
+ * Author: Sweena Jeyagugan
+ *
+ * Date created: 10/06/2021
+ * Description: This will allow the user to add details when they make a new user account
+ */
+
+import WaterDistibution.Controllers.DashBoardController;
 import WaterDistibution.SceneManager;
-import javafx.event.ActionEvent;
+import WaterDistibution.ThemeConfig;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
+public class CreateUserLogin extends Pane{
+    private static final double PREF_WIDTH = 640;
+    private static final double PREF_HEIGHT = 480;
 
-public class CreateUserLogin {
-    String firstName;
-    String lastName;
-    String email;
-    String password;
-    int age;
-    //should username be static? double check with robert.
-    String username;
-    String gender;
-    String location;
+    private VBox primaryBox = new VBox();
+    private HBox hBoxBtnBar = new HBox();
+    private GridPane grid = new GridPane();
+    private Label lblTitle = new Label("Water Distribution: Create Account");
+    private Label lblFirstName = new Label("First Name");
+    private Label lblLastName = new Label ("Last Name");
+    private Label lblDOB = new Label("Date of Birth");
+    private Label lblGender = new Label("Gender");
+    private Label lblEmail = new Label("Email Address");
+    private Label lblAddress = new Label("Address");
+    private TextField firstName = new TextField();
+    private TextField lastName = new TextField();
+    private TextField DOB = new TextField();
+    private TextField Gender = new TextField();
+    private TextField Email = new TextField();
+    private TextField Address = new TextField();
+    private Button butSubmit = new Button ("Submit");
+    private Button butCreateAccount = new Button("Create Account");
 
-    public CreateUserLogin(){}
-    public CreateUserLogin(String firstName,String lastName, String email,String username,String password,int age,String gender, String location){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.age = age;
-        this.username = username;
-        this.gender = gender;
-        this.location = location;
+
+    public CreateUserLogin(){
+        setLayout();
+        setEvents();
+
     }
 
+    private void setEvents() {
+    }
+
+    private void setLayout(){
+        this.setPrefHeight(PREF_HEIGHT);
+        this.setPrefWidth(PREF_WIDTH);
+        this.setHeight(PREF_HEIGHT);
+        this.setWidth(PREF_WIDTH);
 
 
+    }
 
 }
